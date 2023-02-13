64079def0684783bff170b63fc0b9510151602f3
churbaeva <fedosova1997@mail.ru>
2023-02-14 00:08:32 +0500

    3

---
+++ work1/Program.cs
@@ -0,0 +1,14 @@
+﻿ /*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+Console.WriteLine(CountNaturalSum(4,8));
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+    {
+        return n;
+    }
+    return n + CountNaturalSum(m, n - 1);
+}
